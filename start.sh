be213737a70b776e57bcb5d69109f230dd77ff5f
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2022-04-12 21:00:22 +0000

    Update start.sh to show user name

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ echo ~~~~~~~~~~~~~~~~~~
 
 export PATH=$GITPOD_REPO_ROOT/local/python/bin:/workspace/pycon2022-experiments/local/wabt:$PATH
 export PYTHONPATH=$GITPOD_REPO_ROOT/local/python
-export PS1="\n\[\e[32m\]\$GITPOD_GIT_USER_EMAIL \[\e[33m\]\w\[\e[0m\]\n\$ "
+export PS1="\n\[\e[32m\][\$GITPOD_GIT_USER_NAME]@gitpod.io\[\e[33m\]\w\[\e[0m\]\n\$ "

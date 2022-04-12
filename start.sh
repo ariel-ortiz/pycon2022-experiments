02b9f35cf6fe869ce9b63d736c12ca623afc315c
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2022-04-12 18:45:31 +0000

    Update start.sh file

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
-export PATH=/workspace/pycon2022-experiments/local/python/bin:/workspace/pycon2022-experiments/local/wabt:$PATH
-export PYTHONPATH=/workspace/pycon2022-experiments/local/python
+clear
+
+echo ~~~~~~~~~~~~~~~~~~
+echo PyCon USA 2022
+echo ~~~~~~~~~~~~~~~~~~
+
+export PATH=$GITPOD_REPO_ROOT/local/python/bin:/workspace/pycon2022-experiments/local/wabt:$PATH
+export PYTHONPATH=$GITPOD_REPO_ROOT/local/python
 export PS1="\n\[\e[32m\]\$GITPOD_GIT_USER_EMAIL \[\e[33m\]\w\[\e[0m\]\n\$ "
